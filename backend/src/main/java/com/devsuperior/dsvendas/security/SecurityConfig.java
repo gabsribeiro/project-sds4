51b257370690374211062dbdb8f7270cd926354d
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2021-09-12 14:45:44 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dsvendas/security/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsvendas/security/SecurityConfig.java
@@ -21,20 +21,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private Environment env;
 
-	// LIBERA O APP DE GERENCIAMENTO DO H2
+	// ACESS TO THE H2 DB MANAGEMENT APP
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
 
-		// API REST SEM ESTADO
+		// API REST STATELESS
 		http.cors().and().csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.authorizeRequests().anyRequest().permitAll();
 	}
 
-	// DÁ PERMISSÃO PARA O FRONT-END
+	// FRONT-END ACCESS PERMISSION
 	@Bean
 	CorsConfigurationSource corsConfiguretionSource() {
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
