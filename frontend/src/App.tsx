1361bdfe11d2f2c2e49bb7046466b061115fc45c
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2021-09-12 21:03:04 -0300

    DonutChart integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,3 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
 import Routes from "Routes";
 
 function App() {
